b29ad18aa0e80089c59f044fa48eba55ca525cd8
VQMinh <vq.minh97@gmail.com>
2025-04-19 19:20:03 +0700

    Update Map

--- NinjaSurvival/Content/Map/AssestMap.tsx
+++ NinjaSurvival/Content/Map/AssestMap.tsx
@@ -1,4 +1,35 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.2" name="AssestMap" tilewidth="16" tileheight="16" tilecount="4489" columns="67">
+<tileset version="1.10" tiledversion="1.11.0" name="AssestMap" tilewidth="16" tileheight="16" tilecount="4489" columns="67">
  <image source="AssestMap.png" width="1080" height="1080"/>
+ <tile id="753" probability="0.001"/>
+ <tile id="754" probability="0.001"/>
+ <tile id="755" probability="0.001"/>
+ <tile id="756" probability="0.001"/>
+ <tile id="2326" probability="0.001"/>
+ <tile id="2392" probability="0.001"/>
+ <tile id="2393" probability="0.001"/>
+ <tile id="2394" probability="0.001"/>
+ <tile id="2402" probability="0.001"/>
+ <tile id="2403" probability="0.001"/>
+ <tile id="2404" probability="0.001"/>
+ <tile id="2405" probability="0.001"/>
+ <tile id="2860" probability="0.001"/>
+ <tile id="2861" probability="0.001"/>
+ <tile id="2862" probability="0.001"/>
+ <tile id="2863" probability="0.001"/>
+ <tile id="3086" probability="0.001"/>
+ <tile id="3149" probability="0.001"/>
+ <tile id="3152" probability="0.001"/>
+ <tile id="3157" probability="0.001"/>
+ <tile id="3158" probability="0.001"/>
+ <tile id="3159" probability="0.001"/>
+ <tile id="3216" probability="0.001"/>
+ <tile id="3229" probability="0.001"/>
+ <tile id="3292" probability="0.001"/>
+ <tile id="3293" probability="0.001"/>
+ <tile id="3294" probability="0.001"/>
+ <tile id="3295" probability="0.001"/>
+ <tile id="3296" probability="0.001"/>
+ <tile id="3358" probability="0.001"/>
+ <tile id="3359" probability="0.001"/>
 </tileset>
